1fd096c6153bf08ae991b1a28a35a96bc0b613cf
toortanvir2000 <toortanvir2000@gmail.com>
2026-08-11 10:42:13 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums: number[], val: number): number {
+        if (nums.length === 0) return 0;
+        if (!nums.includes(val)) return nums.length;
+        let valAtIdx: number = nums.indexOf(val);
+        let replaceWithIdx: number = valAtIdx;
+        while (replaceWithIdx < nums.length) {
+            if (nums[replaceWithIdx] !== val) {
+                [nums[valAtIdx], nums[replaceWithIdx]] = [nums[replaceWithIdx], nums[valAtIdx]];
+                valAtIdx++;
+            }
+            replaceWithIdx++;
+        }
+        console.log(nums);
+        return nums.indexOf(val) ?? nums.length;
+    }
+}
